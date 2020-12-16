67e9b382d115004ffba83aa68cd7ce2ac2ee7bce
Perttu Raivio <raivio@iki.fi>
2020-12-16 20:20:29 +0200

    RepositoryItem and RepositoryList added

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const RepositoryItem = ({ item }) => {
+  const {
+    fullName,
+    description,
+    language,
+    stargazersCount,
+    forksCount,
+    reviewCount,
+    ratingAverage,
+  } = item;
+  return (
+    <>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </>
+  );
+};
+
+export default RepositoryItem;
